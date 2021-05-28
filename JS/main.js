cf86ddfefc460f706bca89cc774b847c9ed04dad
BitSayian <francoisgreeff16@gmail.com>
2021-05-28 21:16:04 +0200

    created basic layout with a bit of content  and added downloading feature

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+
+let clickable = document.getElementById('resume')
+if(clickable){
+    clickable.addEventListener('click', ()=>{
+        console.log('called');
+       let tempElement = document.createElement('a');
+       tempElement.setAttribute('href', 'JS/Francois_Greeff_CV.docx');
+       tempElement.setAttribute('download','Francois Greeff CV.docx');
+       document.body.appendChild(tempElement);
+       tempElement.click();
+       document.body.removeChild(tempElement);
+
+    })
+}
+else{
+    console.log('null');
+}
